b46bf8fd0f7566de19b1409b4a186dd122420da4
Nurbek Bektursyn <125189291+nurbektursyn@users.noreply.github.com>
2026-03-15 20:19:57 +0500

    Update streamlit_app.py
    Added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests  
+import requests 
+import pandas
 
 # Write directly to the app
 st.title(f":cup_with_straw: Example Streamlit App :cup_with_straw:")
@@ -20,7 +21,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')), col('SEARCH_ON')
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
@@ -37,8 +38,10 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
         #st.text(smoothiefroot_response.json())
-        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        #st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         #st.write(ingredients_string)
+        pd_df = my_dataframe.to_pandas()
+        st.dataframe(pd_df)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
