bde262ae1a031b32845bcedf391cfd75e0b83f86
Nurbek Bektursyn <125189291+nurbektursyn@users.noreply.github.com>
2026-03-15 20:28:56 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie")
 st.write("The name on your smoothie will be:", name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')), col('SEARCH_ON')
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 pd_df = my_dataframe.to_pandas()
 st.dataframe(pd_df)
 #st.dataframe(data=my_dataframe, use_container_width=True)
